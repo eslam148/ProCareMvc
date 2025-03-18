f011910823747e9a154635dae7e51fa96d15a013
eslam148 <islam.18333388@compit.aun.edu.eg>
2025-03-19 01:27:05 +0200

    add Database Classes

--- ProCareMvc.Database/AppDbContext.cs
+++ ProCareMvc.Database/AppDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProCareMvc.Database.Entity;
 
 namespace ProCareMvc.Database
 {
-   public class AppDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    public class AppDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
         public AppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
 
---
+++ ProCareMvc.Database/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCareMvc.Database
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime ModifiedAt { get; set; } = DateTime.Now;
+
+
+
+    }
+}
