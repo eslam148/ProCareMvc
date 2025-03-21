941326b2ac58a2e22e21dff18a57e9f5faadfa8d
AbdelrahamnSaeed <abdosaeed792@gmail.com>
2025-03-21 16:40:18 +0200

    Add Class Order

--- ProCareMvc.Database/Entity/Order/Order.cs
+++ ProCareMvc.Database/Entity/Order/Order.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +11,19 @@ namespace ProCareMvc.Database.Entity
 {
     public class Order
     {
-        public int Id { get; set; }
+        public int TransactionId { get; set; } //Number of order
+
+        [ForeignKey("Patient")]
+        public int PatientId { get; set; }
+
+        public string PaymentMethod { get; set; }
+        
         public decimal TotalPrice { get; set; }
+        public DateTime DateOrder {  get; set; }
+        public string Status {  get; set; }
+
+        public List<OrderItem> orderItems { get; set; }
+        public virtual Patient Patient { get; set; }
+
     }
 }
