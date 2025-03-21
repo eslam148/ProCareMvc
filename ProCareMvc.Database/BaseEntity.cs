9aea8cdf50c4fb08910290f99a4dc277f8d81662
eslam148 <islam.18333388@compit.aun.edu.eg>
2025-03-21 14:57:12 +0200

    last Update

--- ProCareMvc.Database/BaseEntity.cs
+++ ProCareMvc.Database/BaseEntity.cs
@@ -14,7 +14,6 @@ namespace ProCareMvc.Database
         public string Name { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime ModifiedAt { get; set; } = DateTime.Now;
-
         public bool IsDeleted { get; set; } = false;
 
 
---
+++ ProCareMvc.Database/Entity/Order/AppountmentOrderItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCareMvc.Database.Entity.Order
+{
+    public class AppountmentOrderItem:OrderItem
+    {
+    }
+}
