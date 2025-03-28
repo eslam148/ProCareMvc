c1c6d77671fb4d3d578251a53557ce018d2c64a9
eslam148 <islam.18333388@compit.aun.edu.eg>
2025-03-28 02:26:54 +0200

    update

---
+++ ProCareMvc.Database/Utility/StaticInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCareMvc.Database.Utility
+{
+    public static class StaticInfo
+    {
+        public static string DefultImage { get; set; } = "1.png";
+    }
+
+}
--- ProCareMvc.presentation/Controllers/AccountController.cs
+++ ProCareMvc.presentation/Controllers/AccountController.cs
@@ -1,12 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProCareMvc.Database.Entity;
 
 namespace ProCareMvc.presentation.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _userManager; // استخدام User بدلاً من ApplicationUser
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(model);
+
+        //    }
+
+
+        //    var user = await _userManager.GetUserAsync(User);
+        //    if (user == null)
+        //    {
+        //        RedirectToAction("Index", "Home");
+        //    }
+
+
+        //    var result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+        //    if (result.Succeeded)
+        //    {
+
+        //        await _signInManager.RefreshSignInAsync(user);
+        //        return RedirectToAction("Index", "Home");
+        //    }
+        //    foreach (var error in result.Errors)
+        //    {
+        //        ModelState.AddModelError(string.Empty, error.Description);
+        //    }
+        //    return View(model);
+
+
+
+        //}
+
     }
 }
