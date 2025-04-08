67744112f7cce886d8eabd5dbf41b8fe833b4bd7
abdelrahmanabdelnaser123 <abdoamen4447@gmail.com>
2025-04-08 03:23:16 +0200

    add

---
+++ ProCareMvc.presentation/Controllers/ShowDoctorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProCareMvc.presentation.Controllers
+{
+    public class ShowDoctorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
