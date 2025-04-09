c482c77121b1091ac3020843c0b729d46a67f18a
AbdelrahamnSaeed <abdosaeed792@gmail.com>
2025-04-09 03:54:07 +0200

    Craete PH , Update, Details And index

--- ProCareMvc.presentation/Controllers/ShowController.cs
+++ ProCareMvc.presentation/Controllers/ShowController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ProCareMvc.business;
-using ProCareMvc.Database.Entity;
-using ProCareMvc.presentation.Models;
+﻿using Microsoft.AspNetCore.Mvc;//
+using Microsoft.EntityFrameworkCore;//
+using ProCareMvc.business;//
+using ProCareMvc.Database.Entity;//
+using ProCareMvc.presentation.Models;//
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ProCareMvc.presentation.Controllers
 {
@@ -9,13 +11,15 @@ namespace ProCareMvc.presentation.Controllers
     {
         private readonly IUnitOfWork unitOfWork;
 
-        public ShowController(IUnitOfWork unitOfWork) {
+        public ShowController(IUnitOfWork unitOfWork)
+        {
             this.unitOfWork = unitOfWork;
         }
-        public IActionResult IndexPH()
+        public IActionResult ShowPatientHistoryList()
         {
             List<PatientHestoryVM> hestoryVMs = unitOfWork.PatientHestory
-                .GetAll().Select( ph => new PatientHestoryVM() {
+                .GetAll().Select(ph => new PatientHestoryVM()
+                {
                     Id = ph.Id,
                     Diagnosis = ph.Diagnosis,
                     Treatment = ph.Treatment,
@@ -28,7 +32,7 @@ namespace ProCareMvc.presentation.Controllers
         }
 
         // GET: ShowController/Details/5
-        public async Task<ActionResult> DetailsPH(Guid id)
+        public async Task<ActionResult> ShowDetailsOfPatientHistory(Guid id)
         {
             PatientHestory patnthstryFrmDB = await unitOfWork.PatientHestory.GetByIdAsync(id);
             if (patnthstryFrmDB == null)
@@ -47,8 +51,132 @@ namespace ProCareMvc.presentation.Controllers
             };
             return View(patenthstryVM);
         }
-        
 
+        [HttpGet]
+        public IActionResult CreatePatientHistoryRecord()
+        {
+            var usersList = unitOfWork.Patient.GetAll()
+                .Include(p => p.User)
+                .Select(p => new SelectListItem
+                {
+                    Value = p.UserId.ToString(),
+                    Text = p.User.FirstName + " " + p.User.LastName,
+                }).ToList();
 
+            PatientHestoryVM patientHestory = new PatientHestoryVM()
+            {
+                UsersList = usersList
+            };
+            return View(patientHestory);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePatientHistoryRecord(PatientHestoryVM model)
+        {
+            if (ModelState.IsValid && model.NextAppointment > model.VisitDate)
+            {
+                PatientHestory patientHestoryToDB = new PatientHestory()
+                {
+                    Diagnosis = model.Diagnosis,
+                    Treatment = model.Treatment,
+                    Medication = model.Medication,
+                    VisitDate = model.VisitDate,
+                    NextAppointment = model.NextAppointment,
+                    UserId = model.UserId,
+                };
+                await unitOfWork.PatientHestory.InsertAsync(patientHestoryToDB);
+                unitOfWork.Save();
+                return RedirectToAction(nameof(ShowPatientHistoryList));
+            }
+
+            var usersList = unitOfWork.Patient.GetAll()
+                .Include(p => p.User)
+                .Select(p => new SelectListItem
+                {
+                    Value = p.UserId.ToString(),
+                    Text = p.User.FirstName + " " + p.User.LastName,
+                }).ToList();
+
+            PatientHestoryVM patientHestory = new PatientHestoryVM()
+            {
+                UsersList = usersList
+            };
+            return View(model);
+        }
+
+        public async Task<ActionResult> EditPatientHistoryRecord(Guid id)
+        {
+            PatientHestory? patientHestoryFromDB = await unitOfWork.PatientHestory
+                .GetByIdAsync(id);
+
+            if (patientHestoryFromDB == null)
+            {
+                return NotFound();
+            }
+
+            var usersList = unitOfWork.Patient.GetAll()
+                .Include(p => p.User)
+                .Select(p => new SelectListItem
+                {
+                    Value = p.UserId.ToString(),
+                    Text = p.User.FirstName + " " + p.User.LastName
+                }).ToList();
+
+            PatientHestoryVM model = new PatientHestoryVM()
+            {
+                Id = patientHestoryFromDB.Id,
+                UserId = patientHestoryFromDB.UserId,
+                Diagnosis = patientHestoryFromDB.Diagnosis,
+                Treatment = patientHestoryFromDB.Treatment,
+                Medication = patientHestoryFromDB.Medication,
+                VisitDate = patientHestoryFromDB.VisitDate,
+                NextAppointment = patientHestoryFromDB.NextAppointment,
+                UsersList = usersList
+            };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditPatientHistoryRecord(Guid id, PatientHestoryVM modelFromReq)
+        {
+            if (ModelState.IsValid && modelFromReq.NextAppointment > modelFromReq.VisitDate)
+            {
+                PatientHestory? patientHestoryFromDB = await unitOfWork.PatientHestory
+                    .GetByIdAsync(id);
+
+                if (patientHestoryFromDB == null)
+                {
+                    return NotFound();
+                }
+
+                patientHestoryFromDB.Id = modelFromReq.Id;
+                patientHestoryFromDB.UserId = modelFromReq.UserId;
+                patientHestoryFromDB.Diagnosis = modelFromReq.Diagnosis;
+                patientHestoryFromDB.Treatment = modelFromReq.Treatment;
+                patientHestoryFromDB.Medication = modelFromReq.Medication;
+                patientHestoryFromDB.VisitDate = modelFromReq.VisitDate;
+                patientHestoryFromDB.NextAppointment = modelFromReq.NextAppointment;
+
+                await unitOfWork.PatientHestory.ExecuteUpdateAsync(patientHestoryFromDB);
+                unitOfWork.Save();
+
+                return RedirectToAction(nameof(ShowPatientHistoryList));
+            }
+            else
+            {
+                var usersList = unitOfWork.Patient.GetAll()
+                .Include(p => p.User)
+                .Select(p => new SelectListItem
+                {
+                    Value = p.UserId.ToString(),
+                    Text = p.User.FirstName + " " + p.User.LastName
+                }).ToList();
+
+                modelFromReq.UsersList = usersList;
+
+                return View(modelFromReq);
+            }
+        }
     }
 }
--- ProCareMvc.presentation/Models/PatientHestoryVM.cs
+++ ProCareMvc.presentation/Models/PatientHestoryVM.cs
@@ -1,4 +1,6 @@
-﻿using ProCareMvc.Database.Entity;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ProCareMvc.Database.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProCareMvc.presentation.Models
 {
@@ -11,12 +13,15 @@ namespace ProCareMvc.presentation.Models
 
         public string Medication { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime VisitDate { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime NextAppointment { get; set; }
 
         public Guid UserId { get; set; }
 
         public User? User { get; set; }
+        public List<SelectListItem> UsersList { get; set; }
     }
 }
