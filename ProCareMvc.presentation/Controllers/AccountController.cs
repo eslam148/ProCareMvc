9124bf05dce370fa19b237e573c7c2af5d956ee7
eslam148 <islam.18333388@compit.aun.edu.eg>
2025-04-09 20:22:58 +0200

    same mod

--- ProCareMvc.presentation/Controllers/AccountController.cs
+++ ProCareMvc.presentation/Controllers/AccountController.cs
@@ -138,7 +138,103 @@ public class AccountController(IUnitOfWork unitOfWork, SignInManager<User> signI
         return BadRequest();
     }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterVM userFromReq)
+    {
+        if (ModelState.IsValid)
+        {
+            // 1) add database
+            User user = new User();
+            user.Email = user.UserName = userFromReq.Email;
 
+            IdentityResult result = await userManager.CreateAsync(user, userFromReq.Password);
+            if (result.Succeeded)
+            {
+                // 2) Add To cookies
+                await userManager.AddToRoleAsync(user, "Admin");
+                await _signInManager.SignInAsync(user, false);    //create cookies => Login
+                return RedirectToAction("Login", "Account");
+            }
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+        }
+        return View("Register", userFromReq);
+    }
+
+    public IActionResult PatientRegister()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> PatientRegister(PatientRegisterVM PatientFromReq)
+    {
+        if (ModelState.IsValid)
+        {
+            // 1) add database
+            User user = new User();
+
+            user.FirstName = PatientFromReq.FirstName;
+            user.LastName = PatientFromReq.LastName;
+            user.Email = user.UserName = PatientFromReq.Email;
+            user.PhoneNumber = PatientFromReq.PhoneNumber;
+            user.BirthDate = PatientFromReq.BirthDate;
+            user.Gender = PatientFromReq.Gender;
+
+
+
+            IdentityResult result = await userManager.CreateAsync(user, PatientFromReq.Password);
+            if (result.Succeeded)
+            {
+                Patient patient = new Patient();
+                patient.UserId = user.Id;
+
+                // 2) Add To cookies
+                //await userManager.AddToRoleAsync(userFromDb, "Admin");
+                //await signInManeg.SignInAsync(user, false);    //create cookies
+                await unitOfWork.Patient.InsertAsync(patient);
+                return RedirectToAction("Login", "Account");
+            }
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+        }
+        return View("Register", PatientFromReq);
+    }
+
+
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> PatientLogin(LoginVM userFromReq)
+    {
+        if (ModelState.IsValid)
+        {
+            User? userFromDb = await userManager.FindByEmailAsync(userFromReq.Email);
+            if (userFromDb != null)
+            {
+                bool found = await userManager.CheckPasswordAsync(userFromDb, userFromReq.Password);
+                if (found)
+                {
+                    await _signInManager.SignInAsync(userFromDb, userFromReq.RememberMe);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Invalid email or password");
+        }
+        return View("Login", userFromReq);
+    }
+
+    public async Task<IActionResult> LogOut()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Login");
+    }
 
 
 }
