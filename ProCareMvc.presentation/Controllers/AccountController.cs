b39ab0fc5f036b445c756e6f758086667a2b3649
eslam148 <islam.18333388@compit.aun.edu.eg>
2025-04-11 23:28:51 +0200

    git add

--- ProCareMvc.presentation/Controllers/AccountController.cs
+++ ProCareMvc.presentation/Controllers/AccountController.cs
@@ -237,7 +237,33 @@ public class AccountController(IUnitOfWork unitOfWork, SignInManager<User> signI
         }
         return View("Login", userFromReq);
     }
+    public IActionResult ForgetPassword()
+    {
+        return View();
+    }
 
+    [HttpPost]
+    public async Task<IActionResult> ForgetPassword(ForgetPasswordViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View(model);
+
+        var user = await _userManager.FindByEmailAsync(model.Email);
+
+        if (user == null)
+        {
+            ModelState.AddModelError(string.Empty, "No user found with this email.");
+            return View(model);
+        }
+
+        var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+        var resetPasswordLink = Url.Action("ResetPassword", "Account", new { token, email = model.Email }, Request.Scheme);
+
+        //  await _emailService.SendEmailAsync(model.Email, "Reset your password", $"Click here to reset: {resetPasswordLink}");
+
+        ViewBag.Message = "If the email exists, a reset link has been sent.";
+        return View(model);
+    }
     public async Task<IActionResult> LogOut()
     {
         await _signInManager.SignOutAsync();
